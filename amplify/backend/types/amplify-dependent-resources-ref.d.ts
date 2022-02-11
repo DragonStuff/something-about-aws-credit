e1739b31fca4f6abdd6cd771db475e2c4b9fcb87
Alexander <4584443+DragonStuff@users.noreply.github.com>
2022-02-11 11:10:25 +0000

    changes for v2

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "somethingaboutawscre69a488b3": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
